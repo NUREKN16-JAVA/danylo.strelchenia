2cd4eadc4163bbcbb7b4cdbdd78a4862ba12b0d0
DanyloStrelchenia <45274653+DanyloStrelchenia@users.noreply.github.com>
2018-12-28 13:11:15 +0200

    Add files via upload

---
+++ usermanagement/src/test/java/ua/nure/kn/yavorovenko/usermanagement/db/MockUserDao.java
@@ -0,0 +1,58 @@
+package ua.nure.kn.strelchenya.usermanagement.db;
+
+import ua.nure.kn.strelchenya.usermanagement.User;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MockUserDao implements UserDao {
+
+    private Long id;
+    private Map<Long, User> users;
+
+    public MockUserDao() {
+        users = new HashMap<>();
+        id = 0L;
+    }
+
+    @Override
+    public User create(User user) throws DatabaseException {
+        Long currentId = ++id;
+        user.setId(currentId);
+        users.put(currentId, user);
+        return user;
+    }
+
+    @Override
+    public void update(User user) throws DatabaseException {
+        Long currentId = user.getId();
+        users.replace(currentId, user);
+    }
+
+    @Override
+    public void delete(User user) throws DatabaseException {
+        Long currentId = user.getId();
+        users.remove(currentId);
+    }
+
+    @Override
+    public User find(Long id) throws DatabaseException {
+        return users.get(id);
+    }
+
+    @Override
+    public Collection<User> find(String firstName, String lastName) throws DatabaseException {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public Collection<User> findAll() throws DatabaseException {
+        return users.values();
+    }
+
+    @Override
+    public void setConnectionFactory(ConnectionFactory connectionFactory) {
+
+    }
+}
